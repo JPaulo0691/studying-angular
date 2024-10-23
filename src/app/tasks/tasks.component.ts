9b7102736083cbe5ef8023a3ed91a0f54d5bfe58
JPaulo0691 <joaopauloaacosta@gmail.com>
2024-10-23 14:41:08 -0300

    manipulating ngModel in todoList App

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskData } from './new-task/new-task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
@@ -13,6 +15,7 @@ export class TasksComponent {
 
   @Input({required: true}) name!: string;
   @Input({required:true}) userId!: string;
+  isAddingTask = false;
 
   tasks = DUMMY_TASKS;
 
@@ -20,4 +23,28 @@ export class TasksComponent {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 
+  onCompleteTask(id: string){
+
+   this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData){
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+
+    this.isAddingTask = false;
+  }
 }
